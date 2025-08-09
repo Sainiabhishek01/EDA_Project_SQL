91af6aed3f5958c189f8f5f9c2c01bd3f1ac98be
Sainiabhishek01 <abhis6219@gmail.com>
2025-08-09 09:51:16 +0100

    Update 07_Change_Over_Time_Analysis.sql

--- scripts/07_Change_Over_Time_Analysis.sql
+++ scripts/07_Change_Over_Time_Analysis.sql
@@ -18,10 +18,15 @@ SQL Functions Used:
 Select 
 Year(Order_Date) As Order_Year,
 Month(Order_Date) As Order_Month,
-Count(Order_Id) Total_Customers,
+Count(Order_Id) Total_Orders,
+COUNT(DISTINCT CONCAT(First_Name, ' ', Last_Name)) AS Total_Customers, -- For Unique Customers 
 Sum(Quantity) Total_Quantity,
-Round(Sum(Total_Price),2) As Total_Sales
+Round(Sum(Total_Price),2) As Total_Sales,
+Round(Avg(Total_Price),2) As Average_Sales,
+Round(Avg(Price),2) Avg_Price,
+Max(Round(Total_Price,2)) As Highest_Sales,
+Min(Round(Total_Price,2)) As Lowest_Sales
 From Sales.orders
 Where Order_ID Is Not NUll 
-Group By Year(Order_Date),Month(Order_Date) --Format Funcation 
+Group By Year(Order_Date),Month(Order_Date) 
 Order by 1,2
